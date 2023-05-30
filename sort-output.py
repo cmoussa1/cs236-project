ce5ad5a8958334ded10f24e609f3c95ed72f580e
cmoussa1 <moussa1@llnl.gov>
2023-05-30 15:40:37 -0700

    sort-output.py: move extra credit to sep functions
    Add a new data enrichment that sorts and outputs revenue by year. Move
    the two data enrichment sections of code to their own functions in
    sort-output.py and call them after the initial month/year ranking
    output.

--- sort-output.py
+++ sort-output.py
@@ -14,6 +14,121 @@ import sys
 import os
 
 
+def enrich_by_season(revenue_dict):
+    # dictionary that specifies which season a month belongs to
+    seasons = {
+        "fall": [9, 10, 11],
+        "winter": [12, 1, 2],
+        "spring": [3, 4, 5],
+        "summer": [6, 7, 8],
+    }
+
+    # first, we need to move our key-value pairs into their respective
+    # "seasons" bins, as defined by the seasons dictionary above
+    fall = []
+    winter = []
+    spring = []
+    summer = []
+
+    # "for every key in the dictionary"
+    for i in revenue_dict:
+        month_year = i.split("-")
+        if int(month_year[0]) in seasons["fall"]:
+            fall.append(i)
+        elif int(month_year[0]) in seasons["winter"]:
+            winter.append(i)
+        elif int(month_year[0]) in seasons["spring"]:
+            spring.append(i)
+        elif int(month_year[0]) in seasons["summer"]:
+            summer.append(i)
+
+    # now, add up the revenue for each season
+    fall_revenue = 0.0
+    for key in fall:
+        fall_revenue += revenue_dict[key]
+
+    winter_revenue = 0.0
+    for key in winter:
+        winter_revenue += revenue_dict[key]
+
+    spring_revenue = 0.0
+    for key in spring:
+        spring_revenue += revenue_dict[key]
+
+    summer_revenue = 0.0
+    for key in summer:
+        summer_revenue += revenue_dict[key]
+
+    seasonal_revenue = {}
+    seasonal_revenue["fall"] = round(float(fall_revenue), 2)
+    seasonal_revenue["winter"] = round(float(winter_revenue), 2)
+    seasonal_revenue["spring"] = round(float(spring_revenue), 2)
+    seasonal_revenue["summer"] = round(float(summer_revenue), 2)
+
+    print(
+        f"max seasonal revenue was in {max(seasonal_revenue, key=lambda key: seasonal_revenue[key])}: ${max(seasonal_revenue.values())}\n"
+    )
+
+    print("seasonal revenue rankings (descending order):")
+    sorted_seasonal_revenue = sorted(seasonal_revenue.items(), key=lambda x: x[1])
+    for season in reversed(sorted_seasonal_revenue):
+        print(f"   {season[0]} - ${season[1]}")
+
+
+def enrich_by_year(revenue_dict):
+    # dictionary of lists to hold key-value pairs by year
+    revenue_by_years = {
+        2015: [],
+        2016: [],
+        2017: [],
+        2018: [],
+    }
+
+    # "for every key in the dictionary"
+    for i in revenue_dict:
+        month_year = i.split("-")
+        if int(month_year[1]) == 2015:
+            revenue_by_years[2015].append(i)
+        elif int(month_year[1]) == 2016:
+            revenue_by_years[2016].append(i)
+        elif int(month_year[1]) == 2017:
+            revenue_by_years[2017].append(i)
+        elif int(month_year[1]) == 2018:
+            revenue_by_years[2018].append(i)
+
+    # now, add up the revenue for each year
+    revenue_2015 = 0.0
+    for key in revenue_by_years[2015]:
+        revenue_2015 += revenue_dict[key]
+
+    revenue_2016 = 0.0
+    for key in revenue_by_years[2016]:
+        revenue_2016 += revenue_dict[key]
+
+    revenue_2017 = 0.0
+    for key in revenue_by_years[2017]:
+        revenue_2017 += revenue_dict[key]
+
+    revenue_2018 = 0.0
+    for key in revenue_by_years[2018]:
+        revenue_2018 += revenue_dict[key]
+
+    yearly_revenue = {}
+    yearly_revenue[2015] = round(float(revenue_2015), 2)
+    yearly_revenue[2016] = round(float(revenue_2016), 2)
+    yearly_revenue[2017] = round(float(revenue_2017), 2)
+    yearly_revenue[2018] = round(float(revenue_2018), 2)
+
+    print(
+        f"max yearly revenue was in {max(yearly_revenue, key=lambda key: yearly_revenue[key])}: ${max(yearly_revenue.values())}\n"
+    )
+
+    print("yearly revenue rankings (descending order):")
+    sorted_yearly_revenue = sorted(yearly_revenue.items(), key=lambda x: x[1])
+    for year in reversed(sorted_yearly_revenue):
+        print(f"   {year[0]} - ${year[1]}")
+
+
 def main():
     # read in output file from the command line
     try:
@@ -57,65 +172,13 @@ def main():
 
     print("output extra credit opportunities\n")
 
-    # extra credit 1: enriching data set
+    # extra credit 1: enriching data set (sorting by season)
+    enrich_by_season(revenue_dict)
 
-    # dictionary that specifies which season a month belongs to
-    seasons = {
-        "fall": [9, 10, 11],
-        "winter": [12, 1, 2],
-        "spring": [3, 4, 5],
-        "summer": [6, 7, 8],
-    }
+    print()
 
-    # first, we need to move our key-value pairs into their respective
-    # "seasons" bins, as defined by the seasons dictionary above
-    fall = []
-    winter = []
-    spring = []
-    summer = []
-
-    # "for every key in the dictionary"
-    for i in revenue_dict:
-        month_year = i.split("-")
-        if int(month_year[0]) in seasons["fall"]:
-            fall.append(i)
-        elif int(month_year[0]) in seasons["winter"]:
-            winter.append(i)
-        elif int(month_year[0]) in seasons["spring"]:
-            spring.append(i)
-        else:
-            summer.append(i)
-
-    # now, add up the revenue for each season
-    fall_revenue = 0.0
-    for key in fall:
-        fall_revenue += revenue_dict[key]
-
-    winter_revenue = 0.0
-    for key in winter:
-        winter_revenue += revenue_dict[key]
-
-    spring_revenue = 0.0
-    for key in spring:
-        spring_revenue += revenue_dict[key]
-
-    summer_revenue = 0.0
-    for key in summer:
-        summer_revenue += revenue_dict[key]
-
-    seasonal_revenue = {}
-    seasonal_revenue["fall"] = round(float(fall_revenue), 2)
-    seasonal_revenue["winter"] = round(float(winter_revenue), 2)
-    seasonal_revenue["spring"] = round(float(spring_revenue), 2)
-    seasonal_revenue["summer"] = round(float(summer_revenue), 2)
-
-    print(
-        f"max seasonal revenue was in {max(seasonal_revenue, key=lambda key: seasonal_revenue[key])}: ${max(seasonal_revenue.values())}\n"
-    )
-
-    print("seasonal revenue rankings (descending order):")
-    for season in seasonal_revenue:
-        print(f"   {season} - ${seasonal_revenue[season]}")
+    # extra credit 2: enriching data set (sorting by year)
+    enrich_by_year(revenue_dict)
 
 
 if __name__ == "__main__":
