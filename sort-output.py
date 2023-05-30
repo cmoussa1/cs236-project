41851b144d5cfd022b8fe120d83fa2622eecd197
cmoussa1 <moussa1@llnl.gov>
2023-05-30 15:47:36 -0700

    sort-output.py: move main ranking to helper func
    Move the code that sorts the month-year/revenue key-value pairs to its
    own helper function to clean up the code in main().

--- sort-output.py
+++ sort-output.py
@@ -14,6 +14,33 @@ import sys
 import os
 
 
+def enrich_by_month_year(file_object):
+    revenue_dict = {}
+    for line in file_object:
+        # convert line to key-value pair
+        line = "=".join(line.split())
+        tmp_dict = dict(item.split("=") for item in line.split(","))
+
+        # - insert key-value pair into dictionary
+        # - make sure to convert revenue to a float and round to 2 decmial
+        #   places
+        for i in tmp_dict:
+            revenue_dict[i] = round(float(tmp_dict[i]), 2)
+
+    # sort the dictionary that contains all key-value pairs of month/year-revenue
+    # by value (i.e revenue)
+    sorted_revenue = sorted(revenue_dict.items(), key=lambda x: x[1])
+
+    # print out the dictionary in reverse so that the key-value pair with the
+    # highest revenue gets printed out first
+    #
+    # format: month-year: $000.00
+    for month_year in reversed(sorted_revenue):
+        print(f"{month_year[0]}: ${month_year[1]}")
+
+    return revenue_dict
+
+
 def enrich_by_season(revenue_dict):
     # dictionary that specifies which season a month belongs to
     seasons = {
@@ -137,28 +164,7 @@ def main():
         print("Usage: python3 " + os.path.basename(__file__) + " path/to/output_file")
         sys.exit(1)
 
-    revenue_dict = {}
-    for line in file_object:
-        # convert line to key-value pair
-        line = "=".join(line.split())
-        tmp_dict = dict(item.split("=") for item in line.split(","))
-
-        # - insert key-value pair into dictionary
-        # - make sure to convert revenue to a float and round to 2 decmial
-        #   places
-        for i in tmp_dict:
-            revenue_dict[i] = round(float(tmp_dict[i]), 2)
-
-    # sort the dictionary that contains all key-value pairs of month/year-revenue
-    # by value (i.e revenue)
-    sorted_revenue = sorted(revenue_dict.items(), key=lambda x: x[1])
-
-    # print out the dictionary in reverse so that the key-value pair with the
-    # highest revenue gets printed out first
-    #
-    # format: month-year: $000.00
-    for month_year in reversed(sorted_revenue):
-        print(f"{month_year[0]}: ${month_year[1]}")
+    revenue_dict = enrich_by_month_year(file_object)
 
     file_object.close()
 
